9e110a6c3a84d63d5538e200bb9d44849beeb311
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 22:56:55 -0400

    secondary

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
-      configuration_aliases = [ aws.secondary ]
+      configuration_aliases = [ secondary ]
     }
   }
 
