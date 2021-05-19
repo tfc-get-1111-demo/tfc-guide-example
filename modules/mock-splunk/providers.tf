b8dd9aa152fa964fc01598456babc8aa3a0eae77
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 22:35:34 -0400

    please

--- modules/mock-splunk/providers.tf
+++ modules/mock-splunk/providers.tf
@@ -3,18 +3,18 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
-      # configuration_aliases = [ aws.secondary ]
+      configuration_aliases = [ aws.secondary ]
     }
   }
 
   required_version = ">= 0.14.0"
 }
 
-# Default provider
-provider "aws" {
-  profile = "default"
-  region  = var.region
-}
+# # Default provider
+# provider "aws" {
+#   profile = "default"
+#   region  = var.region
+# }
 
 # provider "aws" {
 #   region = var.region_secondary
