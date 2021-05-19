07c14996f8124c916a27fed10a3677566fefaabb
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 23:11:48 -0400

    bye reqd prov

--- modules/mock-splunk/providers.tf
+++ modules/mock-splunk/providers.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-      configuration_aliases = [ aws.secondary ]
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#       configuration_aliases = [ "aws.secondary" ]
+#     }
+#   }
 
-  required_version = ">= 0.14.0"
-}
+#   required_version = ">= 0.14.0"
+# }
 
 # # Default provider
 # provider "aws" {
