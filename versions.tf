48a8fed973bf1cbf6fe292a893eae80492edc77b
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 23:06:41 -0400

    remove reqd providers

--- versions.tf
+++ versions.tf
@@ -1,11 +1,21 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-      configuration_aliases = [ aws.secondary ]
-    }
-  }
-
-  required_version = ">= 0.14.0"
+# Default provider
+provider "aws" {
+  region = var.region_primary
 }
+
+provider "aws" {
+  region = "us-west-2"
+  alias  = "secondary"
+}
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#       configuration_aliases = [ aws.secondary ]
+#     }
+#   }
+
+#   required_version = ">= 0.14.0"
+# }
