bce7556cbdc9049dcb73a003a3fa184b5af3fa57
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 21:14:39 -0400

    ohhh like that

--- main.tf
+++ main.tf
@@ -8,9 +8,12 @@ module "hashi-demo" {
 
 module "hashi-demo-secondary" {
   source          = "./modules/mock-splunk"
-  providers       = [aws.secondary]
   mock_splunk     = var.mock_splunk
   instance_size   = var.instance_size
   subnet_id       = module.hashi-demo-vpc-secondary.public_subnets[0]
   mandatory_tags  = var.mandatory_tags
+  
+  providers = {
+    aws = aws.secondary
+  }
 }
